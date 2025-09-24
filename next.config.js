755283c66092f0c52d1524f899fcabd94b0749cd
jorgef-lopezm <jorgef.lopezm@gmail.com>
2025-09-24 13:43:47 -0600

    Moving redirects and rewrites to _headers and _redirects

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
     env: {
         mode: process.env.NODE_ENV,
     },
@@ -19,36 +24,6 @@ const nextConfig = {
         });
 
         return config;
-    },
-    async redirects() {
-        return [
-            {
-                source: '/graveyard.json',
-                destination: '/api/graveyard',
-                permanent: true,
-            },
-            {
-                source: '/umami.js',
-                destination: '/_next/static/umami.js',
-                permanent: true,
-            }
-        ];
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/_next/static/umami.js',
-                destination: 'https://analytics.bale.media/script.js'
-            },
-            {
-                source: '/api/send',
-                destination: 'https://analytics.bale.media/api/send',
-            },
-            {
-                source: '/social/:path*',
-                destination: 'https://static.killedbygoogle.com/com/social/:path*',
-            }
-        ]
     },
 };
 
